2ba3274f854ebec3fa149200b4155f3dfe052a5b
adriele_farias <adrielemartel029@gmail.com>
2026-07-06 16:23:25 -0300

    feat (produto): alteração do arquivo html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+//import { RouterOutlet } from '@angular/router'; //Remover impotação do RouterOutlet, pois não é necessário para o componente App
+import { Produto } from './components/produto/produto';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Produto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
