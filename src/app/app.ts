6b18693ee453d219026b5283667ec092bda52169
adriele_farias <adrielemartel029@gmail.com>
2026-07-14 17:16:47 -0300

    feat(app): Atualizar a lógica do components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 //!import { RouterOutlet } from '@angular/router'; //Remover impotação do RouterOutlet, pois não é necessário para o componente App
 import { Produto } from './components/produto/produto';
+import { ListaProdutos } from './components/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
 }
+ 
  
