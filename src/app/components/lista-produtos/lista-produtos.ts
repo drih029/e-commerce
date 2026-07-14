eab8ed2d5a93dcc7bbf17e090b8b3fffd749788c
adriele_farias <adrielemartel029@gmail.com>
2026-07-14 17:21:27 -0300

    feat(produto): Implementar lógica e ppropriedades do components produto

--- src/app/components/lista-produtos/lista-produtos.ts
+++ src/app/components/lista-produtos/lista-produtos.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Produto } from '../produto/produto';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [Produto],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
-export class ListaProdutos {}
+export class ListaProdutos {
+  produtos =[
+    {nome: 'Teclado Gamer',preco:49.99},
+    {nome: 'Mouse Gamer',preco: 29.99},
+    {nome: 'Monotor Gamer',preco:599.99},
+    {nome: 'Dekstop Gamer',preco:4999.99},
+    {nome: 'Headset Gamer',preco:699.99}
+ ]
+}
