0d4b0522975f8348ed3ac7ef3dfa4c91bc837735
adriele_farias <adrielemartel029@gmail.com>
2026-07-15 17:09:36 -0300

    refactor: Atualiza modulo da aplicação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 //!import { RouterOutlet } from '@angular/router'; //Remover impotação do RouterOutlet, pois não é necessário para o componente App
-import { Produto } from './components/produto/produto';
-import { ListaProdutos } from './components/lista-produtos/lista-produtos';
+import { Produto } from './features/produtos/produto/produto';
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
   imports: [ListaProdutos],
