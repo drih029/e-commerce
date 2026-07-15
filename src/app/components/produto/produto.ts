43ac4d9a79b26b4aa094e93e396e7a9bbbb2a363
adriele_farias <adrielemartel029@gmail.com>
2026-07-15 13:53:38 -0300

    feat(produto):implementar lógica e propriedades do components produto

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input,Output } from '@angular/core';
 import { UpperCasePipe, CurrencyPipe } from '@angular/common';
 import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
-
 @Component({
   selector: 'app-produto',
   imports: [UpperCasePipe, PrecoFormatadoPipe], 
@@ -9,14 +8,7 @@ import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
   styleUrl: './produto.css',
 })
 export class Produto {
- // produto = 'Notebook';
-//  preco = 2500;
-//  mostrarPreco = true;
-//  mostraProduto = true;
-produtos = [
-  {produto: 'Monitor', preco: 1000},
-  {produto: 'Teclado', preco: 49.99},
-  {produto: 'Mouse', preco: 25.50}
-];
+  @Input() nome: string ='';
+  @Input() preco: number = 0; 
 }   
  
