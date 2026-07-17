997528b10dca3580ab807a45a13b3900462bd2b7
adriele_farias <adrielemartel029@gmail.com>
2026-07-17 16:07:27 -0300

    feat(app):alteração de arquivo

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,21 +1,55 @@
 import { Component } from '@angular/core';
+import { signal } from '@angular/core';
 import { Produto } from '../produto/produto';
+import { computed } from '@angular/core';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
+import { effect } from '@angular/core';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto],
+  imports: [Produto, PrecoFormatadoPipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  produtos =[
+  produtos = signal([
     {nome: 'Teclado Gamer',preco:49.99},
     {nome: 'Mouse Gamer',preco: 29.99},
     {nome: 'Monotor Gamer',preco:599.99},
     {nome: 'Dekstop Gamer',preco:4999.99},
     {nome: 'Headset Gamer',preco:699.99}
- ]
+ ]);
  exibirProduto (nome: string){
-  console.log ('Produto Selecionado:', nome);
+ // console.log ('Produto Selecionado:', nome);
+ this.produtoSelecionado.set(nome);
  }
+adicionarProduto(){
+  this.produtos.update(listaAtual => [
+    ...listaAtual, 
+     {nome: 'Playstation 5 Pro', preco: 10000}]);
+}
+totalProdutos = computed(() => this.produtos().length);
+
+valorTotal = computed(() => { return this.produtos().reduce
+  ((total, item) => total + item.preco, 0)});
+
+  substituirProdutos() {
+    this.produtos.set([
+      {nome: 'Arroz Fazenda', preco: 400},
+    ]);
+  } 
+   constructor() {
+    effect(() => {
+      console.log('Lista de produtos Alterados: ', this.produtos());
+    });
+    effect(() => {
+      console.log('Valor total atualizado: ',  this.valorTotal());
+    });
+    effect(() => {
+      if (typeof document !== 'undefined'){
+       document.title = `Produtos (${this.totalProdutos()}) Minha Loja`; 
+      }
+    });
+  } 
+  produtoSelecionado = signal <string | null>(null);
 }
