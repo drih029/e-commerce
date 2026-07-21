91eb995365d22ef5c5cb272ac8b379c3a5062e20
adriele_farias <adrielemartel029@gmail.com>
2026-07-21 15:37:58 -0300

    feat:novo import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,16 @@
 import { Component, signal } from '@angular/core';
-//!import { RouterOutlet } from '@angular/router'; //Remover impotação do RouterOutlet, pois não é necessário para o componente App
+import { RouterOutlet,RouterLink } from '@angular/router'; //Remover impotação do RouterOutlet, pois não é necessário para o componente App
 import { Produto } from './features/produtos/produto/produto';
 import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja= ' Martel Tech';//nome do e-commerce
 }
  
   
