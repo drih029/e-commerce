b280181966fc54f71379c88bcc5e7a1760dd0e18
adriele_farias <adrielemartel029@gmail.com>
2026-07-22 14:02:17 -0300

    feat: alteração de arquivo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet,RouterLink } from '@angular/router'; //Remover impotação do RouterOutlet, pois não é necessário para o componente App
-import { Produto } from './features/produtos/produto/produto';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+//import { RouterOutlet } from '@angular/router'; //Remover impotação do RouterOutlet, pois não é necessário para o componente App
+//import { Produto } from './features/produtos/produto/produto';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja= ' Martel Tech';//nome do e-commerce
+  nomeLoja= 'Mercado liso';
 }
  
   
