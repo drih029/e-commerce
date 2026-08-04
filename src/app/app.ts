5abff67317db750cf7d4c26df643b2048da4a256
adriele_farias <adrielemartel029@gmail.com>
2026-08-04 17:07:24 -0300

    refactor: Adicionei um arquivo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet} from '@angular/router';
 import { Produto } from './features/produtos/produto/produto';
 import { usuarioLogado,login,logout } from './core/auth';
-import { UpperCasePipe } from '@angular/common';
+import { Header } from './shared/layout/header/header';
 //import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink, UpperCasePipe],
+  imports: [RouterOutlet,Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'Eletrônica Martel'; //nome do e-commerce
   usuarioLogado = usuarioLogado;
   login = login;
   logout = logout;
