726d0445c827e085f22218a36b977541f7967ad3
adriele_farias <adrielemartel029@gmail.com>
2026-08-04 17:05:44 -0300

    refactor: Adicionei um arquivo

---
+++ src/app/shared/layout/header/header.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
+import { RouterLink } from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbarModule,MatButtonModule,MatIconModule,RouterLink,UpperCasePipe],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  nomeLoja = 'Pewiccon'
+}
