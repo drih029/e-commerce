42410d6adfa5b0a6a032db80ce5a5b349357992d
adriele_farias <adrielemartel029@gmail.com>
2026-08-06 15:06:27 -0300

    feat:realização de import

---
+++ src/app/core/services/carrinho.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, signal } from "@angular/core";
+import { Signal } from "@angular/core";
+import { computed } from "@angular/core";
+import { single } from "rxjs";
+@Injectable({
+  providedIn:'root'
+})
+export class CarrinhoService {
+    //! Estado Global
+    private carrinho = signal<{nome: string; preco: number}[]>([]);
+    //? Seletores
+    itens = computed (()=> this.carrinho());
+    quantidadesdeItens= computed (() => this.carrinho().length); //!Quantidades
+    totalItens = computed(()=>
+   this.carrinho().reduce((total,item)=>total+item.preco,0)
+);
+//TODO: Ações
+adicionar(produto:{nome:string; preco:number}){
+    this.carrinho.update(lista => [...lista, produto ]);
+}
+limpar(){
+    this.carrinho.set([]);
+}
+}
