de313100db4803ac0d0e51f89333a69a19a2449e
adriele_farias <adrielemartel029@gmail.com>
2026-08-06 15:06:08 -0300

    feat:realização de import

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon'
 import { RouterLink } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
+import { Inject } from '@angular/core';
+import { CarrinhoService } from '../../../core/services/carrinho.service';
 @Component({
   selector: 'app-header',
   imports: [MatToolbarModule,MatButtonModule,MatIconModule,RouterLink,UpperCasePipe],
@@ -12,4 +14,6 @@ import { UpperCasePipe } from '@angular/common';
 })
 export class Header {
   nomeLoja = 'Martel'
+  private carrinhoService = inject(CarrinhoService);
+  quantidade = this.carrinhoService.quantidadesdeItens;
 }
